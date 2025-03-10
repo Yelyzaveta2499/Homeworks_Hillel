b9063668d169d60c7d3d1eebc89080fdcec616d3
LizaBG <glushichliza@gmail.com>
2025-03-10 22:25:49 +0000

    Homework 2 push

---
+++ Homework 2 My-App/src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int a = 42;
+        int b = 5;
+
+        int result = a+b;
+        System.out.println("a + b = " + result);
+
+
+    }
+}
