04afb06a819584ec3f5a94a26c976a125f469517
LizaBG <glushichliza@gmail.com>
2025-03-18 13:49:43 +0000

    HW5

---
+++ Homework 5/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your income: ");
+        double income = scanner.nextDouble();
+        double tax = calculateTax(income);
+
+        System.out.println("Your tax amount is: " + tax);
+
+        scanner.close();
+    }
+
+    public static double calculateTax(double income) {
+        double tax = 0.0;
+
+        if (income <= 10000) {
+            tax = income * 0.025;
+        } else if (income <= 25000) {
+            tax = 10000 * 0.025 + (income - 10000) * 0.043;
+        } else {
+            tax = 10000 * 0.025 + 15000 * 0.043 + (income - 25000) * 0.067;
+        }
+
+        return tax;
+    }
+}
